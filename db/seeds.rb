ff01c05f8a2d62bcac0959dd3ce9903525a848d6
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-09 21:19:44 +0530

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(name: 'Juarez')
+Food.create!(name: 'Beef', measurement_unit: 'g', quantity: 5000, price: 700, user_id: 1)
+Food.create!(name: 'Rice', measurement_unit: 'g', quantity: 10000, price: 200, user_id: 1)
+Food.create!(name: 'Potato', measurement_unit: 'g', quantity: 5000, price: 150, user_id: 1)
+Food.create!(name: 'Eggs', measurement_unit: 'unit', quantity: 500, price: 150, user_id: 1)
+Food.create!(name: 'Tomato', measurement_unit: 'g', quantity: 5000, price: 100, user_id: 1)
+Food.create!(name: 'Onion', measurement_unit: 'g', quantity: 4000, price: 80, user_id: 1)
+Food.create!(name: 'Garlic', measurement_unit: 'g', quantity: 2000, price: 60, user_id: 1)
+Recipe.create!(name: 'Bife a Cavalo', preparation_time: 10, cooking_time: 20, description: 'Bife a Cavalo is a traditional Brazilian dish consisting of a steak, rice, french fries and a fried egg.', public: true, user_id: 1)
+RecipeFood.create!(quantity: 300, food_id: 1, recipe_id: 1)
+RecipeFood.create!(quantity: 350, food_id: 2, recipe_id: 1)
+RecipeFood.create!(quantity: 100, food_id: 3, recipe_id: 1)
+RecipeFood.create!(quantity: 1, food_id: 4, recipe_id: 1) 
+RecipeFood.create!(quantity: 150, food_id: 5, recipe_id: 1)
+RecipeFood.create!(quantity: 200, food_id: 6, recipe_id: 1)
+RecipeFood.create!(quantity: 50, food_id: 7, recipe_id: 1)
+
