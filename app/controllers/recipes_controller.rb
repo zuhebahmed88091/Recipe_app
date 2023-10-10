8ceba9bbf78b0f158e8635b84beb522221647670
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-10 15:08:48 +0600

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,7 +22,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
-    
+
     if @recipe.save
       redirect_to recipes_path
     else
