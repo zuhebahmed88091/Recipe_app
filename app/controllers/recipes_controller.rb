a15b67c1180334f8d539faee421bf8ef4fba51ca
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-10 15:04:29 +0600

    Update recipe controller with new and create method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,9 +9,30 @@ class RecipesController < ApplicationController
     @recipe_foods = @recipe.recipe_foods
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to recipes_path
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
   end
 end
