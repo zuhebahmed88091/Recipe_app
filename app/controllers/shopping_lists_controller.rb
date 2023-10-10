7cadee5cf8f441969d93055d0e68e71e87f62462
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-10 20:22:12 +0600

    Add shopping lists controller

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,6 @@
+class ShoppingListsController < ApplicationController
+    def index
+        @recipe_foods = current_user.recipe_foods.group(:food_id).sum(:quantity)
+        @foods = current_user.foods
+    end
+end
