756739a625dafe573a767cafb777d67346cdc65c
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-10 15:04:53 +0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :foods, only: [:index, :show, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :destroy] do
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
     resources :recipe_foods, only: [:create] 
   end
   resources :users, only: [:index, :show, :new, :create, :destroy]
